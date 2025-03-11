e16ae6c113e37cbff7eb916703f796a02b79b512
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-11 10:05:38 +0300

    Update and rename ud_09.cpp to using_directive_10.cpp

---
+++ using_directive/using_directive_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+namespace erg
+{
+	int x = 10;
+}
+
+int x = 20;
+
+int main()
+{
+	int x = 100;
+
+	using namespace erg;
+
+	std::cout << "x = " << x << '\n';	//name hiding
+}
