6b9b5f8f5c0f9ba959051fee94cd43e8a821c0ab
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-07 18:01:08 +0300

    Create overloading_07.cpp

---
+++ overloading/overloading_07.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace lib {
+    inline namespace v1 {
+        void process(double) 
+        {
+            std::cout << "v2::process(double)\n";
+        }
+    }
+
+    inline namespace v2 
+    {
+        void process(int) 
+        {
+            std::cout << "v1::process(int)\n";
+        }
+    }
+}
+
+
+int main()
+{
+    using namespace lib;
+
+    lib::process(42);
+    lib::process(3.14);
+}
